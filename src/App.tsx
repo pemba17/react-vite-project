1e70f76d47e155d11771fe9962ebbabe575f7367
pemba17 <pemba.nuru59@gmail.com>
2025-04-22 16:09:38 +0930

    small change in content

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ function App() {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <h1 className="text-2xl font-bold text-blue-800">React + Vite + Tailwind + Docker</h1>
+        <h1 className="text-2xl font-bol">
+          <ul>
+              Stack I am Using
+              <li> - React</li>
+              <li> - Vite</li>
+              <li> - TaiwindCSS</li>
+              <li> - Docker</li>
+          </ul>
+        </h1>
       </div>
     </>
   )
