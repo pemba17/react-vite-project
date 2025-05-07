a3a08eb18dd2d41be4e4cc9a927785f772197081
pemba17 <pemba.nuru59@gmail.com>
2025-05-07 19:16:07 +0930

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <p className="text-2xl font-bold">Work In Progress</p>
+      <p className="text-2xl font-bold">I am working on react</p>
     </div>
   )
 }
